c533942e60f81590e83d52d7487878f67583c69b
nuhtan <ncrichardson15@gmail.com>
2021-03-14 11:40:21 -0400

    finalized basic structure

--- src/core/server.rs
+++ src/core/server.rs
@@ -1,11 +1,6 @@
-use std::{
-    io::Error,
-    io::{BufRead, BufReader, Write},
-    net::{IpAddr, TcpListener, TcpStream},
-    thread,
-};
+use std::{io::Error, io::{BufRead, BufReader, Read, Write}, net::{IpAddr, TcpListener, TcpStream}, thread};
 
-use super::constant::serve_file::generate_request;
+use super::{constant::serve_file::{self, generate_response}, response::{self, Response}};
 
 pub const HTML_PATH: &str = "public";
 
@@ -21,39 +16,44 @@ impl Server {
         }
     }
 
-    pub fn listen(self) -> Result<ExitState, Error> {
+    pub fn listen(self) -> Result<(), Error> {
         for stream_res in self.listener.incoming() {
             let stream = stream_res?;
             let handle = thread::spawn(move || Server::handle_stream(stream));
-            let exit = handle.join().unwrap();
+            let _exit = handle.join().unwrap();
         }
-        Ok(ExitState::Close)
+        Ok(())
     }
 
-    pub fn handle_stream(mut stream: TcpStream) -> Result<ExitState, Error> {
+    pub fn handle_stream(mut stream: TcpStream) -> Result<(), Error> {
         let mut stream_reader = BufReader::new(stream.try_clone()?);
-        let mut line = String::new();
-        stream_reader.read_line(&mut line)?;
+        let mut buf = String::new();
+        stream_reader.read_line(&mut buf).unwrap();
+        // let line = std::str::from_utf8(buf.as_slice()).unwrap();\
+        let line = buf;
         let method = &line[0..line.find(" ").unwrap()];
         let request = &line[line.find("/").unwrap()..line.find("HTTP").unwrap() - 1];
         println!("[{}]: {} {}", stream.peer_addr().unwrap(), method, request,);
-        let response = generate_request(
-            match (method, request) {
-                ("GET", req) => match req {
-                    "/" => "index.html",
-                    _ => "not_found.html",
-                },
-                _ => "not_found.html",
-            },
-            HTML_PATH,
-        )
-        .unwrap();
-        stream.write_all(response.to_block().as_slice())?;
-        return Ok(ExitState::Close);
+        let response = gather_response(method, request);
+        stream.write_all(response.to_block().as_slice()).ok();
+        stream.shutdown(std::net::Shutdown::Both).unwrap();
+        Ok(())
     }
+
+    
 }
 
-pub enum ExitState {
-    Close,
-    Crash,
+pub fn gather_response<'a>(method: &str, request: &'a str) -> Response<'a> {
+    match (method, request) {
+        ("GET", req) => 
+            match generate_response(match req {
+                "/" => "index.html",
+                "/api" => "explanation.html",
+                _ => req
+            }, HTML_PATH) {
+                Ok(res) => res,
+                Err(res) => res
+            },     
+        _ => serve_file::generate_response("not_found.html", HTML_PATH).unwrap()
+    }   
 }
