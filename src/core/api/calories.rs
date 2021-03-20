edb69411ebae2b9057784dc80e72066a2d0f8711
nuhtan <ncrichardson15@gmail.com>
2021-03-19 23:59:38 -0400

    untested api function

--- src/core/api/calories.rs
+++ src/core/api/calories.rs
@@ -1,20 +1,15 @@
-// use std::fs;
+use crate::core::{response, state_data::{state::State}};
 
-// fn read_from_file() -> Vec<u32> {
-//     fs::read_to_string("").unwrap();
-
-// }
-
-// fn add_day()
-
-
-
-// This is what a line should be
-// {
-//     count: num,
-//     date: yyyy/mm/dd,
-//     items: [
-//         item: calories,
-//         ...
-//     ]
-// }
+pub fn add_day<'a>(
+    body: String,
+    shared_data: State,
+) -> Result<response::Response<'a>, response::Response<'a>> {
+    match serde_json::from_str(body.as_str()) {
+        Ok(cal) => {
+            let mut state = shared_data.calories.lock().unwrap();
+            state.push(cal);
+            Ok(response::Response::<'a>::new(201, "application/json", String::from("{\'status\': \'completed'}")))
+        },
+        Err(_) => Err(response::Response::<'a>::new(400, "application/json", String::from("{\'message\': \'invalid data, could not deserialize\'}")))
+    }
+}
