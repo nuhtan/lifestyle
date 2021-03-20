17e8e82cd55d9b95aaef66c2398349b39e16532b
nuhtan <ncrichardson15@gmail.com>
2021-03-20 13:42:49 -0400

    started adding tests

--- src/core/api/calories.rs
+++ src/core/api/calories.rs
@@ -21,3 +21,35 @@ pub fn add_day<'a>(
         )),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{add_day, State};
+
+    #[test]
+    fn test_correct_same_ordering() {
+        let state = State::new([0, 0, 0, 0], 8020);
+        let body = String::from("{\"index\":1,\"total\":200,\"date\":[2021,3,18],\"burn\":300,\"food\":[[\"Steak\",500]]}");
+        let response = String::from_utf8(add_day(body, state).unwrap().to_block()).unwrap();
+        assert_eq!(response, String::from("HTTP/1.1 201 Created\r\nContent-Type: application/json\r\n\r\n{\'status\': \'completed\'}"), "Error found");
+    }
+
+    #[test]
+    fn test_correct_incorrect_ordering() {
+        let state = State::new([0, 0, 0, 0], 8020);
+        let body = String::from("{\"index\":1,\"date\":[2021,3,18],\"food\":[[\"Steak\",500]],\"burn\":300,\"total\":200}");
+        let response = String::from_utf8(super::add_day(body, state).unwrap().to_block()).unwrap();
+        assert_eq!(response, String::from("HTTP/1.1 201 Created\r\nContent-Type: application/json\r\n\r\n{\'status\': \'completed\'}"), "Error found");
+    }
+
+    #[test]
+    fn test_incorrect() {
+        let state = State::new([0, 0, 0, 0], 8020);
+        let body = String::from("{\"index\":1,\"dudes\":[2021,3,18],\"food\":[[\"Steak\",500]],\"burn\":300,\"total\":200}");
+        let result = super::add_day(body, state);
+        assert!(result.is_err());
+        let response =
+            String::from_utf8(result.expect_err("This should be an error").to_block()).unwrap();
+        assert_eq!(response, String::from("HTTP/1.1 400 Bad Request\r\nContent-Type: application/json\r\n\r\n{\'message\': \'invalid data, could not deserialize\'}"));
+    }
+}
--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -8,11 +8,35 @@ pub fn apply_request<'a>(
     shared_data: State,
 ) -> Result<response::Response<'a>, response::Response<'a>> {
     match path {
-        "/api/calorie/add/day" => calories::add_day(body, shared_data),
+        "/api/calories/add/day" => calories::add_day(body, shared_data),
         _ => Err(response::Response::new(
             404,
             "application/json",
             String::from("{\'message\':\'requested resource not found\'}"),
         )),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{apply_request, State};
+
+    #[test]
+    fn apply_correct() {
+        let state = State::new([0, 0, 0, 0], 8020);
+        let body = String::from("{\"index\":1,\"total\":200,\"date\":[2021,3,18],\"burn\":300,\"food\":[[\"Steak\",500]]}");
+        let response = String::from_utf8(apply_request("/api/calories/add/day", body, state).unwrap().to_block()).unwrap();
+        assert_eq!(response, String::from("HTTP/1.1 201 Created\r\nContent-Type: application/json\r\n\r\n{\'status\': \'completed\'}"), "This should work");
+    }
+
+    #[test]
+    fn apply_incorrect_path() {
+        let state = State::new([0, 0, 0, 0], 8020);
+        let body = String::from("{\"index\":1,\"total\":200,\"date\":[2021,3,18],\"burn\":300,\"food\":[[\"Steak\",500]]}");
+        // let response = String::from_utf8(apply_request("/api/calorie/add/day", body, state).unwrap().to_block()).unwrap();
+        let result = apply_request("/api/calorie/add/day", body, state);
+        assert!(result.is_err());
+        let response = String::from_utf8(result.expect_err("This should be an error").to_block()).unwrap();
+        assert_eq!(response, String::from("HTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\n\r\n{\'message\':\'requested resource not found\'}"), "This should work");
     }
 }
