3d420f120364d2665026e5853f6f4b6c3931697f
nuhtan <ncrichardson15@gmail.com>
2021-03-19 23:59:53 -0400

    start of api call parsing

--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -1,1 +1,10 @@
+use super::{response, state_data::state::State};
+
 pub mod calories;
+
+pub fn apply_request<'a>(path: &'a str, body: String, shared_data: State) -> Result<response::Response<'a>, response::Response<'a>> {
+    match path {
+        "/api/calorie/add/day" => calories::add_day(body, shared_data),
+        _ => Err(response::Response::new(404, "application/json", String::from("{\'message\':\'requested resource not found\'}")))
+    }
+}
