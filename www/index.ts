faaf47d8fecf3c11e5cf8ddb21330ee598c68ccf
nuhtan <ncrichardson15@gmail.com>
2021-03-23 17:59:57 -0400

    added the start of typescript

---
+++ www/index.ts
@@ -0,0 +1,16 @@
+void function swap_page(page: Number) {
+    switch(page) {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
+}
