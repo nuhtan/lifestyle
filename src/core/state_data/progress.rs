64a846f4eba637445500642116fd0f15066747ff
nuhtan <ncrichardson15@gmail.com>
2021-03-24 17:07:26 -0400

    struct with load

---
+++ src/core/state_data/progress.rs
@@ -0,0 +1,33 @@
+use std::{fs, io::{BufRead, BufReader}};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct Progress {
+    in_progress: Vec<ToDo>
+}
+
+impl Progress {
+    pub fn load() -> Progress {
+        let file = fs::File::open("database/progress.txt").unwrap();
+        let mut progress = Vec::new();
+        for line in BufReader::new(file).lines() {
+            let line = line.unwrap();
+            let pro: ToDo = serde_json::from_str(line.as_str()).unwrap();
+            progress.push(pro);
+        }
+        Progress {
+            in_progress: progress
+        }
+    }
+
+    pub fn save() {
+
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+pub enum ToDo {
+    Bug((bool, String)),
+    Feature((bool, String))
+}
