d914ac553b2a700c650ebfdbf1f69694982b51e7
nuhtan <ncrichardson15@gmail.com>
2021-03-24 01:27:27 -0400

    a struct for basic data

--- src/core/state_data/mod.rs
+++ src/core/state_data/mod.rs
@@ -1,2 +1,22 @@
 pub mod calories;
 pub mod state;
+
+use std::{fs, io::{BufRead, BufReader, Read}};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct Basic {
+    pub weight_goal: u32,
+    pub rank_goal: u32
+}
+
+impl Basic {
+    pub fn load() -> Basic {
+        let file = fs::File::open("database/calories.txt").unwrap();
+        let mut content = String::new();
+        BufReader::new(file).read_to_string(&mut content).unwrap();
+        let base: Basic = serde_json::from_str(content.as_str()).unwrap();
+        base
+    }
+}
