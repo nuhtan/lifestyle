ff71709fd406ab1540675fe3324924552b85548a
nuhtan <ncrichardson15@gmail.com>
2021-03-24 01:26:38 -0400

    formatting

--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -1,6 +1,7 @@
 use super::{response, state_data::state::State};
 
 pub mod calories;
+pub mod populated;
 
 pub fn apply_request<'a>(
     path: &'a str,
@@ -8,6 +9,7 @@ pub fn apply_request<'a>(
     shared_data: State,
 ) -> Result<response::Response<'a>, response::Response<'a>> {
     match path {
+        // "/api/current" => get_current(shared_data),
         "/api/calories/add/day" => calories::add_day(body, shared_data),
         _ => Err(response::Response::new(
             404,
@@ -17,6 +19,10 @@ pub fn apply_request<'a>(
     }
 }
 
+// fn get_current<'a>(shared_data: State) -> Result<response::Response<'a>, response::Response<'a>> {
+    
+// }
+
 #[cfg(test)]
 mod tests {
     use super::{apply_request, State};
@@ -25,7 +31,12 @@ mod tests {
     fn apply_correct() {
         let state = State::new([0, 0, 0, 0], 8020);
         let body = String::from("{\"index\":1,\"total\":200,\"date\":[2021,3,18],\"burn\":300,\"food\":[[\"Steak\",500]]}");
-        let response = String::from_utf8(apply_request("/api/calories/add/day", body, state).unwrap().to_block()).unwrap();
+        let response = String::from_utf8(
+            apply_request("/api/calories/add/day", body, state)
+                .unwrap()
+                .to_block(),
+        )
+        .unwrap();
         assert_eq!(response, String::from("HTTP/1.1 201 Created\r\nContent-Type: application/json\r\n\r\n{\'status\': \'completed\'}"), "This should work");
     }
 
@@ -35,7 +46,8 @@ mod tests {
         let body = String::from("{\"index\":1,\"total\":200,\"date\":[2021,3,18],\"burn\":300,\"food\":[[\"Steak\",500]]}");
         let result = apply_request("/api/calorie/add/day", body, state);
         assert!(result.is_err());
-        let response = String::from_utf8(result.expect_err("This should be an error").to_block()).unwrap();
+        let response =
+            String::from_utf8(result.expect_err("This should be an error").to_block()).unwrap();
         assert_eq!(response, String::from("HTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\n\r\n{\'message\':\'requested resource not found\'}"), "This should work");
     }
 }
