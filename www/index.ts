44868aaa310da900eec58e6bc4cf03ac4767e782
nuhtan <ncrichardson15@gmail.com>
2021-03-27 00:10:40 -0400

    switching for toggles and tracking current state

--- www/index.ts
+++ www/index.ts
@@ -1,3 +1,5 @@
+var currentPage;
+
 function fetch_data() {
     // let storage = window.localStorage;
     // let data = fetch('/api/current');
@@ -11,6 +13,7 @@ function fetch_data() {
 function swap_page(page: Number) {
     let title = document.getElementById("pageTitle");
     let body = document.getElementById("pageBody");
+    currentPage = page;
     switch (page) {
         case 0:
             title.innerHTML = "Goal Status";
@@ -20,7 +23,6 @@ function swap_page(page: Number) {
                     body.innerHTML = contents;
                 });
             });
-            page = 0;
             break;
         case 1:
             title.innerHTML = "Calorie Tracking";
@@ -30,7 +32,6 @@ function swap_page(page: Number) {
                     body.innerHTML = contents;
                 });
             });
-            page = 1;
             break;
         case 2:
             title.innerHTML = "Long Term Shopping";
@@ -40,6 +41,12 @@ function swap_page(page: Number) {
             break;
         case 4:
             title.innerHTML = "Website Progress";
+            fetch("api/pages/progress").then(response => {
+                let data = response.text();
+                data.then(contents => {
+                    body.innerHTML = contents;
+                });
+            });
             break;
         default:
             break;
@@ -48,9 +55,18 @@ function swap_page(page: Number) {
 
 function choose_modal(page: Number) {
     let body = document.getElementById("modal-body");
+    console.log(page);
     switch (page) {
         case 0:
             fetch("api/pages/modal/calories").then(response => {
+                let data = response.text();
+                data.then(contents => {
+                    body.innerHTML = contents;
+                });
+            });
+            break;
+        case 1:
+            fetch("api/pages/modal/progress").then(response => {
                 let data = response.text();
                 data.then(contents => {
                     body.innerHTML = contents;
@@ -66,18 +82,14 @@ var modal;
 var btn;
 var closeButton;
 
-function modalSetup() {
+function modalSetup(modalPage: Number) {
     modal = document.getElementById("myModal");
     btn = document.getElementById("add");
     closeButton = document.getElementsByClassName("close")[0];
 
     btn.onclick = function () {
         modal.style.display = "block";
-
-        // switch (page) {
-        //     case 1:
-        choose_modal(0);
-        // }
+        choose_modal(modalPage);
     }
 
     closeButton.onclick = function () {
@@ -95,3 +107,14 @@ function removeFood(element) {
     element.parentElement.remove();
 }
 
+function toggleToDo(element) {
+    console.log(element);
+}
+
+function toggleSwitch(element) {
+    if (element.attributes["checked"].value == "false") {
+        element.attributes["checked"].value = "true";
+    } else {
+        element.attributes["checked"].value = "false";
+    }
+}
