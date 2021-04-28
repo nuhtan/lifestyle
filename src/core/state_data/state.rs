152781b7343cbc7c7f3aca8e29463f27c22a5314
nuhtan <ncrichardson15@gmail.com>
2021-04-27 22:03:27 -0400

    fixed generic saving and save progress on close

--- src/core/state_data/state.rs
+++ src/core/state_data/state.rs
@@ -8,10 +8,7 @@ use std::{
 
 use serde::Serialize;
 
-use super::{
-    super::StatefulList, calories::Calories, progress::Progress, shopping_item::ShoppingItem,
-    valorant_game::ValorantGame, Basic,
-};
+use super::{super::StatefulList, Basic, calories::Calories, progress::{Progress, ToDo}, shopping_item::ShoppingItem, valorant_game::ValorantGame};
 
 #[derive(Clone)]
 pub struct State {
@@ -82,38 +79,29 @@ impl State {
 
     pub fn save(self) {
         println!("Saving...");
+        self.progress.lock().unwrap().clone().save();
         // Cloned data for working mutexes
         let second = self.clone();
         let third = self.clone();
         let vec_cals = self.calories.lock().unwrap().clone();
         println!(
             "Wrote {} calorie entries to file.",
-            self.save_generic(vec_cals.clone())
+            save_generic(vec_cals.clone(), "calories.txt")
         );
         let vec_shop = second.shopping.lock().unwrap().clone();
         println!(
             "Wrote {} shopping entries to file.",
-            second.save_generic(vec_shop.clone())
+            save_generic(vec_shop.clone(), "shopping.txt")
         );
         let vec_val = third.valorant.lock().unwrap().clone();
         println!(
             "Wrote {} valorant entries to file.",
-            third.save_generic(vec_val.clone())
+            save_generic(vec_val.clone(), "valorant.txt")
         );
+        
     }
 
-    pub fn save_generic<T: Serialize>(self, list: Vec<T>) -> usize {
-        let file = fs::File::create(format!("database/valorant.txt")).unwrap();
-        let mut writer = LineWriter::new(file);
-        let len = list.len();
-        for item in list.iter() {
-            writer
-                .write_all(serde_json::to_string(&item.clone()).unwrap().as_bytes())
-                .unwrap();
-            writer.write_all(b"\n").unwrap();
-        }
-        return len;
-    }
+    
 
     pub fn verify_files_exist() {
         let files: [&str; 5] = [
@@ -134,4 +122,17 @@ impl State {
             }
         }
     }
+}
+
+pub fn save_generic<T: Serialize>(list: Vec<T>, file_name: &str) -> usize {
+    let file = fs::File::create(format!("database/{}", file_name)).unwrap();
+    let mut writer = LineWriter::new(file);
+    let len = list.len();
+    for item in list.iter() {
+        writer
+            .write_all(serde_json::to_string(&item.clone()).unwrap().as_bytes())
+            .unwrap();
+        writer.write_all(b"\n").unwrap();
+    }
+    return len;
 }
